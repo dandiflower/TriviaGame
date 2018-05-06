8780e118c4df705a5c0a2a5119b598eded49a861
Dandiflower <>
2018-05-06 11:29:45 -0700

    answer function v1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,8 +11,9 @@ $("#done").on("click", done);
 
 
 
+
 // timer object
-var count = 30;
+var count = 20;
 
 function countdown() {
   setTimeout(countdown, 1000); //1000 will run it every 1 second
@@ -24,7 +25,6 @@ function countdown() {
     $("#display").html("You ran out of time!");
   }
 
-//   $("#done").on("click", done);
 }
   
  
@@ -52,9 +52,11 @@ var quiz = [{
     question: "Is Phoenix is the capital of Arizona?", 
     answer: true,
 },{
-    question: "Is Little Rock is the capital of Arkansas?", answer: false,
+    question: "Is Little Rock is the capital of Arkansas?", 
+    answer: false,
 },{
-    question: "Is Sacramento is the capital of California?", answer: true,
+    question: "Is Sacramento is the capital of California?", 
+    answer: true,
 },{
     question: "Is Denver is the capital of Colorado?", 
     answer: false,
@@ -63,11 +65,28 @@ var quiz = [{
 
   // Ask question
 
-  $("#question0").text(quiz[0].question);
-  $("#question1").text(quiz[1].question);
-  $("#question2").text(quiz[2].question);
-  $("#question3").text(quiz[3].question);
-  $("#question4").text(quiz[4].question);
-  $("#question5").text(quiz[5].question);
+  $(".question0").text(quiz[0].question);
+  $(".question1").text(quiz[1].question);
+  $(".question2").text(quiz[2].question);
+  $(".question3").text(quiz[3].question);
+  $(".question4").text(quiz[4].question);
+  $(".question5").text(quiz[5].question);
+
+
+function testCheck() {
+    for (i = 0; i < (quiz.length); i++) {
+        var answersArray = [];
+        var answers = quiz[i].answer;
+        answersArray.push(answers);
+        console.log(answersArray);
+    }
+}  
+
+
+
+testCheck();
+
+// const resultsContainer = document.getElementsByClassName('question');
+// console.log(resultsContainer);
 
 });
