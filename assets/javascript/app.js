5f37e91af0f7d243748096e7ebd279f668745a3a
Dandiflower <>
2018-05-06 15:05:27 -0700

    document ready vs2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,128 +1,99 @@
 // TRIVIA
 // =============================
 
-function myFunction() {
-    console.log("fuck");
-    var x = document.getElementById("myRadio").checked;
-    document.getElementById("demo").innerHTML = x;
-}
+
 
 $(document).ready(function () {
-  
-$("#start").on("click", countdown);
-
-$("#done").on("click", done);
-
+    // when button clicked ...
+    $("#start").on("click", countdown);
+    $("#done").on("click", done);
 
 
 
 
 
-// timer object
-var count = 20;
 
-function countdown() {
-  setTimeout(countdown, 1000); //1000 will run it every 1 second
-  $("#display").html(count);
-  count--;
+    // timer object
+    var count = 20;
 
-  if (count < 0) {
-    //counter ended
-    $("#display").html("You ran out of time!");
-  }
+    function countdown() {
+    setTimeout(countdown, 1000); //1000 will run it every 1 second
+    $("#display").html(count);
+    count--;
 
-}
-  
- 
-// Stops the countdown and ends the game
-
-$("#done").on("click", done);
-
-function done() {
-    count = clearTimeout();
-    $("#display").html("Done! Let's see how you did...");
-    // records user inputs
-    // picks which radio buttons are checked
-   
-
-    
-    
-    //    if (foo.checked === true) {
-    //     console.log(foo);
-    // }
-}
-
-// var myFunction = function () {
-//     // var userInputs = [];
-//     var radioButton = document.getElementById("um").checked;
-//     document.getElementById("display2").innerHTML = radioButton;
-//     console.log(radioButton);
-// };
-
-
-// Quiz array
-
-var quiz = [{
-    question: "Is Montgomery is the capital of Alabama?", 
-    answer: false,
-},{
-    question: "Is Juneau is the capital of Alaska?",
-    answer: true,
-},{
-    question: "Is Phoenix is the capital of Arizona?", 
-    answer: true,
-},{
-    question: "Is Little Rock is the capital of Arkansas?", 
-    answer: false,
-},{
-    question: "Is Sacramento is the capital of California?", 
-    answer: true,
-},{
-    question: "Is Denver is the capital of Colorado?", 
-    answer: false,
-}];
-
-
-  // Ask question
-
-  $(".question0").text(quiz[0].question);
-  $(".question1").text(quiz[1].question);
-  $(".question2").text(quiz[2].question);
-  $(".question3").text(quiz[3].question);
-  $(".question4").text(quiz[4].question);
-  $(".question5").text(quiz[5].question);
-
-// Creates an array of the correct answers 
-function testCheck() {
-    for (i = 0; i < (quiz.length); i++) {
-        var answersArray = [];
-        var answers = quiz[i].answer;
-        answersArray.push(answers);
-        // console.log(answersArray);
+        if (count < 0) {
+            //counter ended
+            $("#display").html("You ran out of time!");
+        }
     }
-}  
+    
+    
+    // Stops the countdown and ends the game
 
-// Create an array of the user selected values
-
-testCheck();
+    $("#done").on("click", done);
 
 
 
-//     var txt = "";
-//     var i;
-//     for (i = 0; i < coffee.length; i++) {
-//         if (coffee[i].checked) {
-//             txt = txt + coffee[i].value + " ";
-//         }
-//     }
-// document.getElementById("order").value = "You ordered a coffee with: " + txt;
 
-    // var test = document.getElementById("um").checked;
-    // console.log(test);
-    // document.getElementById("f").checked = false;
-    // const resultsContainer = document.getElementsByClassName('correct');
-    // console.log(resultsContainer);
+    // Quiz array
+
+    var quiz = [{
+        question: "Is Montgomery is the capital of Alabama?", 
+        answer: false,
+    },{
+        question: "Is Juneau is the capital of Alaska?",
+        answer: true,
+    },{
+        question: "Is Phoenix is the capital of Arizona?", 
+        answer: true,
+    },{
+        question: "Is Little Rock is the capital of Arkansas?", 
+        answer: false,
+    },{
+        question: "Is Sacramento is the capital of California?", 
+        answer: true,
+    },{
+        question: "Is Denver is the capital of Colorado?", 
+        answer: false,
+    }];
+
+
+    // Ask question
+
+    $(".question0").text(quiz[0].question);
+    $(".question1").text(quiz[1].question);
+    $(".question2").text(quiz[2].question);
+    $(".question3").text(quiz[3].question);
+    $(".question4").text(quiz[4].question);
+    $(".question5").text(quiz[5].question);
+
+    // Creates an array of the correct answers 
+    function testCheck() {
+        for (i = 0; i < (quiz.length); i++) {
+            var answersArray = [];
+            var answers = quiz[i].answer;
+            answersArray.push(answers);
+            // console.log(answersArray);
+        }
+    }  
+
+    // Create an array of the user selected values
+
+    testCheck();
+
 
 
 
 });
+
+function userSelection() {
+    var x = document.getElementById("myRadio").checked;
+    document.getElementById("demo").innerHTML = x;
+}
+
+function done() {
+    count = clearTimeout();
+    $("#display").html("Done! Let's see how you did...");
+}
+        // records user inputs
+        // picks which radio buttons are checked
